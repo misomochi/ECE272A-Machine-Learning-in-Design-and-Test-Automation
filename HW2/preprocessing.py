6920abc6194f0f90ed5398cb5870c95ab91ea0a7
misomochi <35426138+misomochi@users.noreply.github.com>
2021-12-14 00:12:08 -0800

    Create preprocessing.py

---
+++ HW2/preprocessing.py
@@ -0,0 +1,27 @@
+import csv
+import numpy as np
+
+def load_data(filename = 'white_wine.csv'):
+	with open(filename, 'r') as file:
+		reader = csv.reader(file)
+		columnNames = next(reader)
+		rows = np.array(list(reader), dtype = float)
+
+	return columnNames, rows
+
+def separate_labels(columnNames, rows):
+	if 'quality' in columnNames:
+		labelColumnIndex = columnNames.index('quality')
+		ys = rows[:, labelColumnIndex]
+		xs = np.delete(rows, labelColumnIndex, axis = 1)
+		del columnNames[labelColumnIndex]
+
+		return columnNames, xs, ys
+	else:
+		return columnNames, rows, []
+
+def save_prediction(filename, columnname, prediction):
+	with open(filename, 'w', newline = '') as file:
+		writer = csv.writer(file)
+		writer.writerow(columnname)
+		writer.writerows(map(lambda x: [x], prediction))
